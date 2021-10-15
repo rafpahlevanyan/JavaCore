a49147e3c33022d553c44bb9b1cebc9d637518cd
rafpahlevanyan <rafok3nt@gmail.com>
2021-10-16 00:07:55 +0400

    lesson5

---
+++ src/lesson5/DoWhile.java
@@ -0,0 +1,11 @@
+package lesson5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do {
+            System.out.println("тaкт " + n);
+            n--;
+        } while (n > 0);
+    }
+}
