19e1736c0e5ded03e34ad2196fa8b349e8a4c9f5
rafpahlevanyan <rafok3nt@gmail.com>
2021-10-29 00:08:54 +0400

    lesson11

---
+++ src/example/SortArray.java
@@ -0,0 +1,23 @@
+package example;
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] array = {3, 7, 8, 6, 12, 85, 66, 33, 42, 10};
+        int n = array.length;
+        int tmp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -1,32 +1,130 @@
 package homework.arrayutil;
 
 public class ArrayUtil {
-    public static void main(String[] args) {
-        int[] array = {5, 7, 9, 6, 1, 13, 24, 33, 55, 79};
-        int i;
+    int max(int[] array) {
         int max = array[0];
-        int min = array[0];
-        int oddCount = 0;
-        int averageValue = 0;
-        for (i = 0; i < array.length; i++) {
-            averageValue+=array[i];
-            System.out.println(array[i]);
-            if (max < array[i]) {
+//        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
                 max = array[i];
             }
-            if (min > array[i]) {
+        }
+        return max;
+    }
+
+    void print(int[] array) {
+        System.out.print("մասիվի թվերն են: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    int min(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
                 min = array[i];
             }
+        }
+        return min;
+    }
+
+    void evens(int[] array) {
+        System.out.print("մասիվի զույգ թվերն են: ");
+        for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                System.out.println(array[i] + " is odd");
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    void evenCount(int[] array) {
+        System.out.print("զույգ թվերի քանակը։ ");
+        int evenCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                evenCount++;
+
+            }
+        }
+        System.out.println(evenCount);
+
+    }
+
+    void odd(int[] array) {
+        System.out.print("մասիվի կենտ թվերն են: ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    void oddCount(int[] array) {
+        System.out.print("կենտ թվերի քանակը։ ");
+        int oddCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
                 oddCount++;
             }
         }
-        System.out.println(max);
-        System.out.println(min);
-        System.out.println("odd Count is " + oddCount);
-        System.out.println("even count is " + (array.length - oddCount));
-        System.out.println("averageValue is " + averageValue/ array.length);
-        System.out.println("sum is "+ averageValue);
+        System.out.println(oddCount);
+    }
+
+    void sum(int[] array) {
+        System.out.print("մասիվի թտվերի գումարը։ ");
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum);
+    }
+
+    void avg(int[] array) {
+        System.out.print("մասիվի միջին թվաբանականը։ ");
+        double avg = 0;
+        for (int i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        System.out.println(avg / array.length);
+    }
+    void sortminmax(int[] array){
+        System.out.print("մասիվը սորտավորված փոքրից-մեծ։ ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] < array[j - 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+
+            }
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+    void sortmaxmin(int[] array){
+        System.out.print("մասիվը սորտավորված մեծից-փոքր։ ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] > array[j - 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+
+            }
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 }
