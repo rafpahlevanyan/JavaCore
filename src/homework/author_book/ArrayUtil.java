3c546521a2fdd086bea78fbb50c7ebb31631cc08
rafpahlevanyan <rafok3nt@gmail.com>
2021-11-29 01:34:38 +0400

    AuthorBook Homework

---
+++ src/homework/author_book/ArrayUtil.java
@@ -0,0 +1,9 @@
+package homework.author_book;
+
+public class ArrayUtil {
+    static void deleteByIndex(Object[] array, int index, int size) {
+        for (int i = index + 1; i < size; i++) {
+            array[i - 1] = array[i];
+        }
+    }
+}
